164d214e999001d19df4d10d875db1a0c853581c
Amanskywalker <ak47su30ac@gmail.com>
2017-02-09 13:10:03 +0530

    new view added to handle request to submit vote  	modified:   website/polls/views.py

--- website/polls/views.py
+++ website/polls/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 #from django.template import loader
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.urls import reverse
+
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:6]
@@ -12,12 +14,21 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def details(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question, pk = question_id)
     return render(request, 'polls/details.html', {'question':question,})
 
 def results(request, question_id):
-    response = "You'r looking at the response of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render (request, 'polls/details.html', {'question':question})
 
 def vote(request, question_id):
-    return HttpResponse("You'r voting on the question %s" % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.post['choice'])
+    except (KeyError, Choice.DoesNotExist)
+        # redisplay the question
+        return render(request, "polls/details.html", {'question':question,'error_message':"You diddn't selected a choice"})
+    else:
+        selected_choice.vote += 1
+        selected_choice.save()
+        return HttpResponseRedirect('polls:results', args = (question.id),)
