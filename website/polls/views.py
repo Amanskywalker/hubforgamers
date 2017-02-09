2c361a0dc960192fed672998b13528c5bfbf9fca
Amanskywalker <ak47su30ac@gmail.com>
2017-02-09 18:37:36 +0530

    modified:   website/polls/views.py

--- website/polls/views.py
+++ website/polls/views.py
@@ -19,16 +19,16 @@ def details(request, question_id):
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
-    return render (request, 'polls/details.html', {'question':question})
+    return render (request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     try:
-        selected_choice = question.choice_set.get(pk = request.post['choice'])
-    except (KeyError, Choice.DoesNotExist)
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
         # redisplay the question
         return render(request, 'polls/details.html', {'question':question,'error_message':"You diddn't selected a choice"})
     else:
-        selected_choice.vote += 1
+        selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect('polls:results', args = (question.id),)
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
